751da50a67e65d1191f149e9bcdb037d780fdb7e
Raderice <eliseybalin@gmail.com>
2024-11-29 14:51:34 +0300

    zxc

--- kyzmenko/sait1Prac/index.js
+++ kyzmenko/sait1Prac/index.js
@@ -1,3 +1,15 @@
 import anime from 'anime';
 import react from 'react';
 import reactDom from 'reactdom';
+
+
+const headerMenu = document.getElementById("headerMenu");
+
+headerMenu.addEventListener('mouseover', openMenu());
+
+function openMenu(){
+    anime({
+        targets: 'headerMenu',
+        translateY: 100,
+    })
+}
